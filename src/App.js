643850dc271016947f301019a14411e3bec29f5e
Paul Lee <paul117@naver.com>
2022-04-21 23:23:19 +0900

    4장 04-2까지 완성된 소스코드

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+function Player({name,picture}) {
+  return (
+    <div>
+      <h2>I like {name}</h2>
+      <img src={picture} />
+    </div>  
+ );
+}
+
+const playerILike = [
+  {
+    name: 'Cristiano Ronaldo',
+    image: 'https://img.sbs.co.kr/newimg/news/20210912/201591158_1280.jpg'
+  },
+  {
+    name: 'Lionel Messi',
+    image: 'http://newsimg.hankookilbo.com/2018/06/27/201806270548089865_4.jpg'
+  },
+  {
+    name: 'Heung-Min Son',
+    image: 'http://www.ejanews.co.kr/news/photo/201711/2017110645376426.jpg'
+  },
+  {
+    name: 'Neymar',
+    image: 'http://db.kookje.co.kr/news2000/photo/2021/0615/L20210615.22018004169i1.jpg'
+  }
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   <div>
+     {playerILike.map(footballer => <Player key={footballer.name} name={footballer.name} picture={footballer.image}/>)}
+   </div>
+  ); 
 }
 
 export default App;
